17a66db0fac26d8e052a323071c9859c85349348
Aakriti0108 <aakritiyadav0108@gmail.com>
2024-03-03 14:46:13 +0530

    restarted

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return (<>
+    <h2>EXPENSE ITEM !!</h2>
+    <div>Food Rs 10</div>
+    <div>Petrol Rs 100</div>
+    <div>Movies Rs200</div>
+    </>
+    
+    )
+}
+
+export default ExpenseItem;
