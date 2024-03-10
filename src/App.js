f498e6a930387a9ef74a14368ce8a59c1bfbe4bc
Aakriti0108 <aakritiyadav0108@gmail.com>
2024-03-10 15:53:59 +0530

    finished part1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
+import Expenses from './components/Expenses/Expenses'
 
-function App() {
+const App=()=> {
 
   const expenses = [
     {
@@ -34,37 +35,7 @@ function App() {
   ]
 
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem 
-      title={expenses[0].title} 
-      amount={expenses[0].amount} 
-      date ={expenses[0].date}
-      LocationOfExpenditure={expenses[0].LocationOfExpenditure}
-      />
-      <ExpenseItem 
-      title={expenses[1].title} 
-      amount={expenses[1].amount} 
-      date ={expenses[1].date}
-      LocationOfExpenditure={expenses[1].LocationOfExpenditure}
-
-      />
-      <ExpenseItem 
-      title={expenses[2].title} 
-      amount={expenses[2].amount} 
-      date ={expenses[2].date}
-      LocationOfExpenditure={expenses[2].LocationOfExpenditure}
-
-      />
-      <ExpenseItem 
-      title={expenses[3].title} 
-      amount={expenses[3].amount} 
-      date ={expenses[3].date}
-      LocationOfExpenditure={expenses[3].LocationOfExpenditure}
-
-      />
-     
-    </div>
+    <Expenses items={expenses}/>
   );
 }
 
--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,15 +1,18 @@
-function ExpenseDate(props){
+import './ExpenseDate.css';
+import Card from '../UI/Card';
+
+const ExpenseDate = (props) =>{
     const month = props.date.toLocaleString('en-US',{month:'long'})
     const day = props.date.toLocaleString('en-US',{day:'2-digit'})
     const year = props.date.getFullYear();
 
    return(
-    <div>
+    <Card className="expense-date">
         <div>{month}</div>
         <div>{year}</div>
         <div>{day}</div>
-    </div>
+    </Card>
    )
 }
 
-export default ExpenseDate
+export default ExpenseDate;
